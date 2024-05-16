ae07cdecedc4cd21b62085f5d04c87796059ed1a
Lautaro <perezlautaro13109@gmail.com>
2024-05-15 21:17:56 -0300

    mi primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Primer commit</title>
+</head>
+<body>
+    <?php
+    print "Mi primera pagina"
+    ?>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim repellat, labore debitis illum aliquam iure odio rem fugiat est itaque nemo delectus temporibus reiciendis. Expedita, hic! Veritatis voluptates expedita hic.
+</body>
+</html>
